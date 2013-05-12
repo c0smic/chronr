dc0f4de41c1be2885e378392d68c817ce6cd9303
Jack Ketcham <kcajketcham@gmail.com>
2013-05-12 16:50:59 -0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chronr::Application.config.secret_token = '2932c5cab8dead7dc90858853e1d82dc413d25359be55bc6f2a0693ab78192533803ccd0a0992cfbfca9eb391c568929a7024a944e76d3e4c4ab5de631cb2966'
